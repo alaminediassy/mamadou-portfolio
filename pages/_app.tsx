8c8e44444c1f57e1d556193f2cb25824bb6fc4c0
alaminediassy <diassy.alamine@gmail.com>
2023-08-20 23:45:28 +0200

    added my navbar section

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
+import { useRouter } from 'next/router'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+  return (
+    <div key={router.pathname}>
+      <Component {...pageProps}/>
+    </div>
+  )
 }
---
+++ public/assets/index.ts
@@ -0,0 +1,5 @@
+import logo from "./images/logo.png";
+import profile from "./images/alamine.jpg";
+
+
+export {logo, profile};
